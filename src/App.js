1eedd56d7eeaad0ed9faf96ca579606529f8e34d
tgafiulin <timur.gafiulin@travelline.ru>
2019-01-12 17:41:01 +0300

    First commit

--- src/App.js
+++ src/App.js
@@ -2,27 +2,32 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import Grid from 'react-bootstrap/lib/Grid';
+import Row from 'react-bootstrap/lib/Row';
+import Col from 'react-bootstrap/lib/Col';
+import styled from 'styled-components'
+
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="wrapper">
+        <SayFullName name='Timur' surname='Gafiulin' link='habr.com' />
+        <SayFullName name='Albert' surname='Gubkin' link='habr.com' />
+        <SayFullName name='Anna' surname='Shamova' link='habr.com' />
       </div>
     );
   }
 }
+
+
+function SayFullName (props) {
+  return (
+      <div>
+        <h1>My name is {props.name}, surname - {props.surname}</h1>
+        <a href={props.link}>Link to my profile</a>
+      </div>
+    )
+}
+
 
 export default App;
